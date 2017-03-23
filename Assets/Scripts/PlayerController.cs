111ee48be7fd7e3548f0b3f941104cc9b4227232
John Hawley <jhawley@institute.local>
2017-03-22 23:37:39 -0500

    plane pitches down and up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,22 @@ public class PlayerController : MonoBehaviour {
 
 	public float shootSpeed = 1;
 
-	public float pitch = 20;
+	//public float pitch = 20;
+
+	private Animator anim;
 
 	private Rigidbody2D body;
 
 	void Start(){
 		body = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float y = Input.GetAxis ("Vertical");
 		body.velocity = Vector2.up * y * speed;
+		anim.SetFloat ("Pitch", y);
 		//transform.rotation.z = y * pitch;
 
 		if (Input.GetButtonDown ("Jump")) {
