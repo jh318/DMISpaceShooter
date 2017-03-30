ea99afbb8b69b6a222f801a9bd98a36e248cf5bc
John Hawley <jhawley@institute.local>
2017-03-29 23:55:22 -0500

    added scrolling script

---
+++ Assets/ScrollMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMaterial : MonoBehaviour {
+
+	Renderer render;
+	Vector2 offset;
+	public float speed = 2.0f;
+
+	void Start(){
+		render = GetComponent<Renderer> ();
+	}
+
+	void Update(){
+		offset.x = offset.x + Time.deltaTime * speed;
+		offset.x %= 1f;
+		render.material.SetTextureOffset ("_MainTex", offset);
+	}
+}
